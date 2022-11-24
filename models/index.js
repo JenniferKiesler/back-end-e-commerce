e26f2e1bc2a3651a51f33cf9eac72ffa667319a6
JenniferKiesler <111549041+JenniferKiesler@users.noreply.github.com>
2022-11-23 22:06:47 -0600

    syncs sequelize and updates the associations between models

--- models/index.js
+++ models/index.js
@@ -4,18 +4,28 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+// Category has many products
 Category.hasMany(Product, {
-  foreignKey: 'category_id',
-  onDelete: 'CASCADE'
-})
-Product.belongsTo(Category, {
   foreignKey: 'category_id'
 })
 
+// Products belong to category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+    onDelete: 'CASCADE'
+})
 
-Product.belongsToMany(Tag, { through: 'ProductTag', onDelete: 'CASCADE'})
+// Products belong to many tags (through ProductTag)
+Product.belongsToMany(Tag, { 
+    through: ProductTag,
+    foreignKey: 'product_id',
+})
 
-Tag.belongsToMany(Product, { through: 'ProductTag', onDelete: 'CASCADE'})
+// Tags belong to many products (through ProductTag)
+Tag.belongsToMany(Product, { 
+    through: ProductTag,
+    foreignKey: 'tag_id',
+})
 
 module.exports = {
   Product,
